10738d12a0f38029058624c444ce01a54ca2f2c6
Matt <matthew.weichselbaum@gmail.com>
2018-02-01 23:56:17 +0100

    ignore dist

--- dist/string_transforms.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function NoOp(str: string): string;
-export declare function CamelCase(str: string): string;
-export declare function SnakeCase(str: string): string;
-export declare function UnderscoreCase(str: string): string;
-export declare function DashCase(str: string): string;
